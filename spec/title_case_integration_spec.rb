1a74f48bf113b91613e117014653fa1e073991fb
Kyle Mellander & Josue Valverde <student@epicodus.com>
2015-08-05 10:27:30 -0700

    Added spec to test that data was returned properly formatted

--- spec/title_case_integration_spec.rb
+++ spec/title_case_integration_spec.rb
@@ -5,14 +5,23 @@ set(:show_exceptions, false)
 
 
 describe('/', {:type => :feature}) do
+
   it("properly opens up the webpage") do
     visit('/')
     expect(page).to have_content(':D')
   end
+
   it("takes us to /title when submit button is pressed") do
     visit('/')
     fill_in('title', :with=>'Hello world')
     click_button('Josue will format your title')
     expect(page).to have_content("Title")
+  end
+
+  it("returns the properly formatted title from title_case method") do
+    visit('/')
+    fill_in('title', :with=>"josue of the jungle")
+    click_button('Josue will format your title')
+    expect(page).to have_content("Josue of the Jungle")
   end
 end
